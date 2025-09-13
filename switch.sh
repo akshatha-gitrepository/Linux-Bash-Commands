92eb2dc62f5a3528acd64fb9faca3c4d7612ed86
akshatha_achar <P1815AS@msc.corpdir.net>
2025-09-13 13:50:42 +0530

    learning switch and while loop

---
+++ switch.sh
@@ -0,0 +1,18 @@
+fruit="apple"
+
+case $fruit in
+   "apple")
+		echo "this is red fruit"
+		;;
+   "banana")
+		echo "this is a yello fruit"
+		;;
+   "orange")
+		echo "this is an orange fruit"
+		;;
+
+   *)
+		echo "unknwn fruit"
+
+		;;
+esac
